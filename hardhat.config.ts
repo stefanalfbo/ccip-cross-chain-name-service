36c9e825c1eee6f436ca7b772692fd8a79bc3532
Stefan Alfbo <stefan@alfbo.se>
2024-08-24 19:44:22 +0200

    Update the repository to the latest dependencies

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,10 +1,9 @@
-import * as dotenvenc from "@chainlink/env-enc"
+import * as dotenvenc from "@chainlink/env-enc";
 dotenvenc.config();
 
-import { HardhatUserConfig, extendEnvironment } from "hardhat/config";
-import { createProvider } from "hardhat/internal/core/providers/construction";
+import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
-import './tasks';
+import "./tasks";
 
 const PRIVATE_KEY = process.env.PRIVATE_KEY;
 const ETHEREUM_SEPOLIA_RPC_URL = process.env.ETHEREUM_SEPOLIA_RPC_URL;
@@ -18,85 +17,93 @@ const KROMA_SEPOLIA_RPC_URL = process.env.KROMA_SEPOLIA_RPC_URL;
 const WEMIX_TESTNET_RPC_URL = process.env.WEMIX_TESTNET_RPC_URL;
 const GNOSIS_CHIADO_RPC_URL = process.env.GNOSIS_CHIADO_RPC_URL;
 const CELO_ALFAJORES_RPC_URL = process.env.CELO_ALFAJORES_RPC_URL;
-
-declare module "hardhat/types/runtime" {
-  export interface HardhatRuntimeEnvironment {
-    changeNetwork: Function
-  }
-}
-
-extendEnvironment(async (hre) => {
-  hre.changeNetwork = async function changeNetwork(newNetwork: string) {
-    hre.network.name = newNetwork;
-    hre.network.config = hre.config.networks[newNetwork];
-    hre.ethers.provider = new hre.ethers.providers.JsonRpcProvider(hre.network.config.url);
-    hre.network.provider = await createProvider(hre.config, newNetwork);
-  }
-})
+const METIS_SEPOLIA_RPC_URL = process.env.METIS_SEPOLIA_RPC_URL;
+const ZKSYNC_SEPOLIA_RPC_URL = process.env.ZKSYNC_SEPOLIA_RPC_URL;
 
 const config: HardhatUserConfig = {
   solidity: "0.8.19",
-  defaultNetwork: 'ethereumSepolia', // Source Chain
   networks: {
     hardhat: {
-      chainId: 31337
+      chainId: 31337,
     },
     ethereumSepolia: {
-      url: ETHEREUM_SEPOLIA_RPC_URL !== undefined ? ETHEREUM_SEPOLIA_RPC_URL : '',
+      url:
+        ETHEREUM_SEPOLIA_RPC_URL !== undefined ? ETHEREUM_SEPOLIA_RPC_URL : "",
       accounts: PRIVATE_KEY !== undefined ? [PRIVATE_KEY] : [],
-      chainId: 11155111
+      chainId: 11155111,
     },
     polygonAmoy: {
-      url: POLYGON_AMOY_RPC_URL !== undefined ? POLYGON_AMOY_RPC_URL : '',
+      url: POLYGON_AMOY_RPC_URL !== undefined ? POLYGON_AMOY_RPC_URL : "",
       accounts: PRIVATE_KEY !== undefined ? [PRIVATE_KEY] : [],
-      chainId: 80002
+      chainId: 80002,
     },
     optimismSepolia: {
-      url: OPTIMISM_SEPOLIA_RPC_URL !== undefined ? OPTIMISM_SEPOLIA_RPC_URL : '',
+      url:
+        OPTIMISM_SEPOLIA_RPC_URL !== undefined ? OPTIMISM_SEPOLIA_RPC_URL : "",
       accounts: PRIVATE_KEY !== undefined ? [PRIVATE_KEY] : [],
       chainId: 11155420,
     },
     arbitrumSepolia: {
-      url: ARBITRUM_SEPOLIA_RPC_URL !== undefined ? ARBITRUM_SEPOLIA_RPC_URL : '',
+      url:
+        ARBITRUM_SEPOLIA_RPC_URL !== undefined ? ARBITRUM_SEPOLIA_RPC_URL : "",
       accounts: PRIVATE_KEY !== undefined ? [PRIVATE_KEY] : [],
-      chainId: 421614
+      chainId: 421614,
     },
     avalancheFuji: {
-      url: AVALANCHE_FUJI_RPC_URL !== undefined ? AVALANCHE_FUJI_RPC_URL : '',
+      url: AVALANCHE_FUJI_RPC_URL !== undefined ? AVALANCHE_FUJI_RPC_URL : "",
       accounts: PRIVATE_KEY !== undefined ? [PRIVATE_KEY] : [],
-      chainId: 43113
+      chainId: 43113,
     },
     bnbChainTestnet: {
-      url: BNB_CHAIN_TESTNET_RPC_URL !== undefined ? BNB_CHAIN_TESTNET_RPC_URL : '',
+      url:
+        BNB_CHAIN_TESTNET_RPC_URL !== undefined
+          ? BNB_CHAIN_TESTNET_RPC_URL
+          : "",
       accounts: PRIVATE_KEY !== undefined ? [PRIVATE_KEY] : [],
-      chainId: 97
+      chainId: 97,
     },
     baseSepolia: {
-      url: BASE_SEPOLIA_RPC_URL !== undefined ? BASE_SEPOLIA_RPC_URL : '',
+      url: BASE_SEPOLIA_RPC_URL !== undefined ? BASE_SEPOLIA_RPC_URL : "",
       accounts: PRIVATE_KEY !== undefined ? [PRIVATE_KEY] : [],
-      chainId: 84532
+      chainId: 84532,
     },
     kromaSepolia: {
-      url: KROMA_SEPOLIA_RPC_URL !== undefined ? KROMA_SEPOLIA_RPC_URL : '',
+      url: KROMA_SEPOLIA_RPC_URL !== undefined ? KROMA_SEPOLIA_RPC_URL : "",
       accounts: PRIVATE_KEY !== undefined ? [PRIVATE_KEY] : [],
-      chainId: 2358
+      chainId: 2358,
     },
     wemixTestnet: {
-      url: WEMIX_TESTNET_RPC_URL !== undefined ? WEMIX_TESTNET_RPC_URL : '',
+      url: WEMIX_TESTNET_RPC_URL !== undefined ? WEMIX_TESTNET_RPC_URL : "",
       accounts: PRIVATE_KEY !== undefined ? [PRIVATE_KEY] : [],
-      chainId: 1112
+      chainId: 1112,
     },
     gnosisChiado: {
-      url: GNOSIS_CHIADO_RPC_URL !== undefined ? GNOSIS_CHIADO_RPC_URL : '',
+      url: GNOSIS_CHIADO_RPC_URL !== undefined ? GNOSIS_CHIADO_RPC_URL : "",
       accounts: PRIVATE_KEY !== undefined ? [PRIVATE_KEY] : [],
-      chainId: 10200
+      chainId: 10200,
     },
     celoAlfajores: {
-      url: CELO_ALFAJORES_RPC_URL !== undefined ? CELO_ALFAJORES_RPC_URL : '',
+      url: CELO_ALFAJORES_RPC_URL !== undefined ? CELO_ALFAJORES_RPC_URL : "",
       accounts: PRIVATE_KEY !== undefined ? [PRIVATE_KEY] : [],
-      chainId: 44787
-    }
-  }
+      chainId: 44787,
+    },
+    metisSepolia: {
+      url: METIS_SEPOLIA_RPC_URL !== undefined ? METIS_SEPOLIA_RPC_URL : "",
+      accounts: PRIVATE_KEY !== undefined ? [PRIVATE_KEY] : [],
+      chainId: 59902,
+    },
+    zksyncSepolia: {
+      url: ZKSYNC_SEPOLIA_RPC_URL !== undefined ? ZKSYNC_SEPOLIA_RPC_URL : "",
+      accounts: PRIVATE_KEY !== undefined ? [PRIVATE_KEY] : [],
+      chainId: 300,
+    },
+  },
+  paths: {
+    sources: "./contracts",
+    tests: "./test",
+    cache: "./cache",
+    artifacts: "./artifacts",
+  },
 };
 
 export default config;
